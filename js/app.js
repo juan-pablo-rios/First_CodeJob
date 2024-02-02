27e90d6fa723a829f82918f7316ac5800b460752
juan-pablo-rios <juanpr3112@gmail.com>
2024-02-01 21:25:43 -0500

    Arreglo del bug en el navbar al iniciar sesión

--- js/app.js
+++ js/app.js
@@ -15,14 +15,14 @@ let openButtonSignInResponsive = document.getElementById(
 function modal() {
   signInModal.style.display = "block";
   toggleMenu();
-};
+}
 
 let signInOptionResponsive = document.getElementById("signInOptionResponsive");
 // FUNCIÓN PARA ABRIR EL MODAL DESDE EL NAVBAR RESPONSIVE:
-signInOptionResponsive.onclick = function() {
-    signInModal.style.display = "block";
-    toggleMenu();
-}
+signInOptionResponsive.onclick = function () {
+  signInModal.style.display = "block";
+  toggleMenu();
+};
 
 // --------------------------------------------- SIGN IN MODAL ---------------------------------------------
 // MODAL:
@@ -48,77 +48,73 @@ window.onclick = function (event) {
 };
 
 // ------------------------------------------------------------------ SIGN IN ------------------------------------------------------------------
+// INICIALIZACIÓN DE VARIABLES CON LOS id DEL TOP BOX:
+let divNavNormal = document.getElementById("topBoxNormal");
+let divNavSignIn = document.getElementById("topBoxSignIn");
+divNavSignIn.style.display = "none";
 
 // FUNCION PARA INICIAR SESIÓN:
 function pressSignIn() {
     // INICIALIZACIÓN DE VARIABLES CON LOS VALORES DE LOS INPUTS:
     let email = document.getElementById("signInEmailInput").value;
     let password = document.getElementById("signInPasswordInput").value;
-
+    
     /*INPUTS DE DEL PROFILE LOS CUALES VAN A HACER CAMBIADOS*/
-    let webSite = document.getElementById("profilePageWeb")
+    let webSite = document.getElementById("profilePageWeb");
     let emailProfile = document.getElementById("profileEmail");
     let telNumberProfile = document.getElementById("profileTelNumber");
-    let country = document.getElementById("country");
+    let city = document.getElementById("city");
     let name = document.getElementById("nameUserProfile");
-    
-    // INICIALIZACIÓN DE VARIABLES CON LOS id DE LOS NAVBAR:
-    let navbarSignIn = document.getElementById("containeMenuSignin");
-    let navbarNormal = document.getElementById("containerMenu");
-    // INICIALIZACIÓN DE VARIABLES CON LOS id DE LAS OPCIONES signIn DEL NAVBAR:
-    let profileOptionResponsive = document.getElementById('burger_menu_item_4');
-    let logOutOptionResponsive = document.getElementById('burger_menu_item_7');
-    let signUpOption = document.getElementById('burger_menu_item_6');
+    // INICIALIZACIÓN DE VARIABLES CON LOS id DE LAS OPCIONES QUE SE DEBEN REMOVER Y AGREGAR AL INICIAR SESIÓN:
+    let profileOptionResponsive = document.getElementById("burger_menu_item_4");
+    let divLogOut = document.getElementById("logout");
+    let divLogged = document.getElementById("logged");
     // INICIALIZACIÓN DE VARIABLE CON EL ID DEL CAMPO A LLENAR CON EL NOMBRE:
-    let putUserName = document.getElementById('userName');
+    let putUserName = document.getElementById("userName");
     // FUNCIÓN fetch() PARA CONECTARSE CON EL json-server Y VERIFICAR LOS DATOS:
     fetch("http://localhost:3000/users")
-    .then(response => response.json())
-    .then(element => {
-        let result = element.filter(function (element) { 
-            return element.email == email && element.password == password
-        })
-        if (result.length > 0) {
-            console.log("ingreso");
-            // CERRAR MODAL:
-            signInModal.style.display = "none";
-            // DESAPARECER NAVBAR NORMAL:
-            navbarNormal.classList.add('displayNone');
-            // HACER APARECER NAVBAR SIGN IN:
-            navbarSignIn.classList.add('displayBlock');
-            // HACER APARECER logOut OPTION:
-            logOutOptionResponsive.classList.add('displayBlock');
-            // HACER APARECER profile OPTION:
-            profileOptionResponsive.classList.add('displayBlock');
-            // HACER DESAPARECER signIn OPTION:
-            signInOptionResponsive.classList.add('displayNone');
-            // HACER DESAPARECER signUp OPTION:
-            signUpOption.classList.add('displayNone');
-            // PONER NOMBRE DEL USUARIO EN EL NAVBAR:
-            putUserName.innerHTML = result[0].name;
-            
-            webSite.textContent = result[0].contact.link;
-            emailProfile.textContent = result[0].contact.mail;
-            telNumberProfile.textContent = result[0].contact.call;
-            country.textContent = result[0].city;
-            name.textContent = result[0].name;
+    .then((response) => response.json())
+    .then((element) => {
+    let result = element.filter(function (element) {
+        return element.contact.mail == email && element.password == password;
+    });
+    // CONDICIONAL PARA COMPROBAR QUE SE ENCONTRÓ UN USUARIO VALIDO:
+    if (result.length > 0) {
+        console.log("ingreso");
+        // CERRAR MODAL:
+        signInModal.style.display = "none";
+        // DESAPARECER NAVBAR NORMAL:
+        divNavNormal.classList.add("displayNone");
+        // APARECER NAVBAR SIGN IN:
+        divNavSignIn.classList.add("displayBlock");
+        // // DESAPARECER OPCIONES DEL NAVBAR RESPONSIVE:
+        // divLogOut.classList.add('displayNone'); /* NO FUNCIONA, NO ELIMINA LAS OPCIONES DEL MENÚ RESPONSIVE CUANDO HACE SIGN IN */
+        // // APARECER OPCIÓN logOut DEL NAVBAR RESPONSIVE:
+        // divLogged.classList.add('displayBlock');
+
+        // INSERTAR NOMBRE DE USUARIO:
+        putUserName.innerHTML = result[0].name;
+
+        // INSERTAR INFORMACIÓN DEL PERFIL:
+        webSite.textContent = result[0].contact.link;
+        emailProfile.textContent = result[0].contact.mail;
+        telNumberProfile.textContent = result[0].contact.call;
+        city.textContent = result[0].city;
+        name.textContent = result[0].name;
+        } else {
+        alert("¡Los datos ingresados no son válidos!");
         }
-        else {
-            alert('¡Los datos ingresados no son válidos!');
-        }
-    })
+    });
 }
 // ------------------------------------------------------------------ SIGN UP ------------------------------------------------------------------
 
 function createUser() {
   // CAPTURAR VALORES DE LOS INPUTS:
-  let signUpNameInput = document.getElementById("signUpNameInput");
-  let signUpEmailInput = document.getElementById("signUpEmailInput");
-  let signUpPhoneInput = document.getElementById("signUpPhoneInput");
-  let signUpPasswordInput = document.getElementById("signUpPasswordInput");
-  let signUpConfirmPasswordInput = document.getElementById(
-    "signUpConfirmPasswordInput"
-  );
+    let signUpNameInput = document.getElementById("signUpNameInput");
+    let signUpEmailInput = document.getElementById("signUpEmailInput");
+    let signUpPhoneInput = document.getElementById("signUpPhoneInput");
+    let signUpPasswordInput = document.getElementById("signUpPasswordInput");
+    let signUpConfirmPasswordInput = document.getElementById("signUpConfirmPasswordInput");
   let signUpDateInput = document.getElementById("signUpDateInput");
   let signUpCityInput = document.getElementById("signUpCityInput");
   let signUpWebsiteInput = document.getElementById("signUpWebsiteInput");
